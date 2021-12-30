ad9f934919be38f9011f74478ff0b242b370dc24
James Ivy <jameskivy@gmail.com>
2021-12-29 20:18:00 -0600

    practicing fundamentals

---
+++ controlflow.py
@@ -0,0 +1,48 @@
+# Comparison Operators - are used to compare values
+
+################################################################
+# boolean express = True or False
+
+# >   = greater than
+# >=  = greater than or equal too
+# <   = less than
+# <=  = less than or equal too
+# ==  = equals
+# !=  = not equal
+
+# "bag" > "apple" == True
+# "bag" == "BAG" == False
+
+# Conditional Statements = time to make a decision 
+# you can have how many elif statements you want
+
+# temperature = 32
+# if temperature > 30:
+#     print("It's warm")
+#     print("Drink water")
+# elif temperature > 20:
+#     print("It's nice")
+# else:
+#     print("It's cold")
+
+# print("Done")
+
+
+# Ternary Operator 
+
+# age = 14
+# if age >= 18:
+#     message = "Eligible"
+# else:
+#     message = "Not eligible"
+
+# print(message)
+
+# refactored - same as above just a cleaner way
+age = 17
+message = "Eligible" if age >= 18 else "Not eligible"
+print(message)
+
+
+
+
