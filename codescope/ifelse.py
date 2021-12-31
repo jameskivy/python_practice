e6c9f985c92809a90052c9da74676680dfb5b4f1
James Ivy <jameskivy@gmail.com>
2021-12-30 22:04:16 -0600

    finished control flow

---
+++ codescope/ifelse.py
@@ -0,0 +1,53 @@
+# A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years.
+# Ask user for their salary and year of service and print the net bonus amount.
+
+# salary = int(input("What is your current salary? "))
+# years_of_service = int(input("How many years have you been with the compnay? "))
+# bonus = .05 * salary
+
+# if years_of_service > 5:
+#     print(f"You will receive a bonus in the amount of {bonus}")
+# else:
+#     print("You will not receive a bonus this time. ")
+
+
+# A school has following rules for grading system:
+# Below 25 - F
+# 26 to 44 - E
+# 45 to 49 - D
+# 50 to 59 - C
+# 60 to 79 - B
+# Above 80 - A
+# Ask user to enter marks and print the corresponding grade.
+
+# marks = int(input("What did you make on the last test? "))
+
+# if marks <= 25:
+#     print("You made an F.")
+# elif marks <= 44 and marks >= 26:
+#     print("You made an E.")
+# elif marks <= 49 and marks >= 45:
+#     print("You made an D.")
+# elif marks <= 59 and marks >= 50:
+#     print("You made an C.")
+# elif marks <= 79 and marks >= 60:
+#     print("You made an B.")
+# elif marks>=80 and marks >= 120:
+#     print("You made an A.")
+
+
+# Ask user to enter age,
+# sex ( M or F ),
+# marital status ( Y or N ) and
+# then using following rules print their place of service.
+# if employee is female, then she will work only in urban areas.
+
+# if employee is a male and age is in between 20 to 40 then he may work in anywhere
+
+# if employee is male and age is in between 40 t0 60 then he will work in urban areas only.
+
+# And any other input of age should print "ERROR".
+
+print ("Enter age ")
+age = input()
+print ("Sex? (M or F) ")
--- controlflow.py
+++ controlflow.py
@@ -13,7 +13,7 @@
 # "bag" > "apple" == True
 # "bag" == "BAG" == False
 
-# Conditional Statements = time to make a decision 
+# Conditional Statements = time to make a decision
 # you can have how many elif statements you want
 
 # temperature = 32
@@ -28,7 +28,7 @@
 # print("Done")
 
 
-# Ternary Operator 
+# Ternary Operator
 #############################################
 
 # age = 14
@@ -47,7 +47,7 @@
 
 # Logical Operators
 # model more complex conditions
-# logical operators are short-circuit meaning - checking one condition at a time 
+# logical operators are short-circuit meaning - checking one condition at a time
 # and = if both conditions are true the result will be true
 # or = at least one condition is true the result will be true
 # not = inverses the value of a boolean, result will be false
@@ -72,7 +72,7 @@
 #     print("Not Eligible")
 
 
-#chaining comparision operators 
+# chaining comparision operators
 # age should be b/w 18 and 65
 
 # age = 10
@@ -86,7 +86,7 @@
 # For Loops
 #########################################
 # there are time we may want to repeat a task a number of times
-# use loops to create repeation 
+# use loops to create repeation
 
 # for number in range(1, 10, 2):
 #     print("Attempt", number, number * ".")
@@ -102,7 +102,7 @@
 #     print("Attempted 3 times and failed")
 
 
-# Nested for loops 
+# Nested for loops
 # a loop inside of another loop
 
 # for x in range(5):
@@ -110,24 +110,62 @@
 #         print(f"({x}, {y})")
 
 
-# Iterables 
+# Iterables
 ########################################################
 # print(type(10))
 # print(type(range(5)))
-#range is an example of complex types in regards to primitive types
-# iterate - we can interate over it
+# range is an example of complex types in regards to primitive types
+# iterate - we can interate over it in for loops
+# strings are also interables
+# lists = used to store a lists of objects
+# for loops are used to iterate over objects
 
 # for x in "Python":
 #     print(x)
 
-name = input('What\'s your name? ')
-uppercase = name.upper()
-letters = len(name)
+# for y in range(5):
+#     print(y)
 
-if name:
-    print(f'HELLO, {uppercase}!')
-    print(f'You have {letters} letters in your name. Dope!')
+# for i in [1, 2, 3, 4, 5]:
+#     print(i)
+
+# While loops - use to repeat as long as the condition is true
+# we are evaluating a condition and repeating a tasks
+
+# number = 100
+# while number > 0:  # condition
+#     print(number)  # print
+#     number //= 2  # divide by half
 
 
+# command = ''
+
+# while command.lower() != 'quit':
+#     command = input('>')
+#     print('ECHO', command)
+
+# infinite loop is a loop that runs forever
+# to end we would need a break statement
+
+# while True:
+#     command = input('>')
+#     print('ECHO', command)
+#     if command.lower() == "quit":
+#         break
+
+
+# EXERCISE
+#####################################################
+
+
+# write a program that displays the even numbers b/w 1 and 10!
+# example 2, 4, 6, 8, 10
+
+# count = 0 
+# for number in range(1, 10):
+#     if number % 2 == 0:
+#         count += 1
+#         print(number)
+# print(f"We have {count} even numbers.")
 
 
